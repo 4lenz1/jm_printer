fdd50bd992fb417905d8dbace6c325addd1de8d0
Fallenzone <pingfallenzone@gmail.com>
2020-10-26 18:13:20 +0900

    initital

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const ePosDev = new epson.ePOSDevice();
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('html loaded');
+
+});
+
+
+function connect() {
+    var ipAddress = '192.168.2.158';
+    var port = '8043';
+    ePosDev.connect(ipAddress, port, callback_connect);
+}
+
+function callback_connect(resultConnect) {
+    var deviceId = 'local_printer';
+    var options = { 'crypto': false, 'buffer': false };
+    if ((resultConnect == 'OK') || (resultConnect == 'SSL_CONNECT_OK')) {
+        //Retrieves the Printer object
+        ePosDev.createDevice(deviceId, ePosDev.DEVICE_TYPE_PRINTER, options,
+            callback_createDevice);
+    }
+    else {
+        //Displays error messages
+    }
+}
+
+var printer = null;
+function callback_createDevice(deviceObj, errorCode) {
+    if (deviceObj === null) {
+        //Displays an error message if the system fails to retrieve the Printer object
+        return;
+    }
+    printer = deviceObj;
+    //Registers the print complete event
+    printer.onreceive = function (response) {
+        if (response.success) {
+            //Displays the successful print message
+        }
+        else {
+            //Displays error messages
+        }
+    };
+}
+
+
+function createData() {
+    printer.addTextAlign(printer.ALIGN_CENTER);
+    printer.addText('Hello World\n');
+}
+
+function send() {
+    if (ePosDev.isConnected) {
+        printer.send();
+    }
+}
+
+//Discards the Printer object
+ePosDev.deleteDevice(printer, callback_deleteDevice);
+function callback_deleteDevice(errorCode) {
+    //Terminates connection with device
+    ePosDev.disconnect();
+}
